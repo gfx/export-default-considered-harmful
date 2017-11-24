62f3565b396391137f01feb21dad8fa477ff742a
FUJI Goro (gfx) <gfuji@cpan.org>
2017-11-24 17:54:39 +0900

    initial commit

---
+++ Makefile
@@ -0,0 +1,4 @@
+
+all:
+	npx ts-node import-with-ts.ts
+	npx babel-node import-with-babel.js
---
+++ import-with-babel.js
@@ -0,0 +1,5 @@
+import React1 from 'react';
+import * as React2 from 'react';
+
+console.log('import X from "react" :', React1);
+console.log('import * as X from "react": ', React2);
